264e44d5e35672624c6f184d5e921ccd5a6b11c8
mora11do <nickbro@byu.edu>
2025-11-11 23:16:03 -0700

    Okay I changed all the naming stuff so that it makes senes with my website. Goodbye scores and hello music stuff stuff

--- service/database.js
+++ service/database.js
@@ -3,9 +3,9 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const musicCollection = db.collection('music');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -18,8 +18,8 @@ const scoreCollection = db.collection('score');
   }
 })();
 
-function getUser(email) {
-  return userCollection.findOne({ email: email });
+function getUser(username) {
+  return userCollection.findOne({ username: username });
 }
 
 function getUserByToken(token) {
@@ -31,20 +31,15 @@ async function addUser(user) {
 }
 
 async function updateUser(user) {
-  await userCollection.updateOne({ email: user.email }, { $set: user });
+  await userCollection.updateOne({ username: user.username }, { $set: user });
 }
 
-async function addScore(score) {
-  return scoreCollection.insertOne(score);
+async function addSong(music) {
+  return musicCollection.insertOne(music);
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
+function getSong(username) {
+  const cursor = musicCollection.find({ username: username });;
   return cursor.toArray();
 }
 
@@ -53,6 +48,6 @@ module.exports = {
   getUserByToken,
   addUser,
   updateUser,
-  addScore,
-  getHighScores,
+  addSong,
+  getSong,
 };
